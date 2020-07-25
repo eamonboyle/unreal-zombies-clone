d279f14452b85c18b7baf8d34aa40f8b3803d0c9
eamonboyle <blaowskate@hotmail.com>
2020-07-25 23:25:54 +0100

    Initial commit

---
+++ Source/ZombiesClone/ZombiesCloneGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombiesCloneGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZombiesCloneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZombiesCloneGameMode();
+};
+
+
+
