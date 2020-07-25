92fae286415a7585b1f3d7e9b79f1a0ab4d6c003
eamonboyle <blaowskate@hotmail.com>
2020-07-26 00:10:12 +0100

    Refactored to character base etc

--- Source/ZombiesClone/ZombiesCloneGameMode.cpp
+++ Source/ZombiesClone/ZombiesCloneGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "ZombiesCloneGameMode.h"
 #include "ZombiesCloneHUD.h"
-#include "ZombiesCloneCharacter.h"
+#include "ZombiesClone/Public/Player/CharacterBase.h"
 #include "UObject/ConstructorHelpers.h"
 
 AZombiesCloneGameMode::AZombiesCloneGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/BlueprintClasses/Player/BP_CharacterBase.BP_CharacterBase_C"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
