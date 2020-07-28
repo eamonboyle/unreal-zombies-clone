8c8f50773fee6da4eaa2f07bd18b70d8fee0697a
eamonboyle <blaowskate@hotmail.com>
2020-07-28 23:47:07 +0100

    Starting to create the player spawn points

---
+++ Source/ZombiesClone/Public/Zombie/Game/ZombiePlayerSpawnPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "ZombiePlayerSpawnPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESCLONE_API AZombiePlayerSpawnPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
