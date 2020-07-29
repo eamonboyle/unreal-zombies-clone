2612a373bd5222ddaf4ddf5946ed048b62b3be93
eamonboyle <blaowskate@hotmail.com>
2020-07-29 12:31:59 +0100

    Create GameState class

---
+++ Source/ZombiesClone/Public/Zombie/Game/ZombieGameState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ZombieGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESCLONE_API AZombieGameState : public AGameStateBase
+{
+    GENERATED_BODY()
+
+public:
+    AZombieGameState();
+};
